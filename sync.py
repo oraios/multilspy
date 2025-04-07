a7d8f1a074013752c2eea74fb45e6532fd3e19bd
Dominik Jain <dominik.jain@outlook.com>
2025-04-08 01:38:46 +0200

    Add repository synchronisation scripts

---
+++ sync.py
@@ -0,0 +1,7 @@
+import os
+from repo_dir_sync import LibRepo, OtherRepo
+
+r = LibRepo()
+r.add(OtherRepo("autobob", "autobob", os.path.join("..", "autobob", "multilspy")))
+r.add(OtherRepo("serena", "serena", os.path.join("..", "serena", "src", "multilspy")))
+r.runMain()
