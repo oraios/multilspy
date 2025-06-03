606542ed8766bfbcc6490b3115eb0aea78c693e5
Dominik Jain <dominik.jain@outlook.com>
2025-06-03 17:20:47 +0200

    Use clean copy of serena (to avoid dealing with all the temporary stuff in language server 'static' dirs)

--- sync.py
+++ sync.py
@@ -3,5 +3,5 @@ from repo_dir_sync import LibRepo, OtherRepo
 
 r = LibRepo()
 r.add(OtherRepo("autobob", "autobob", os.path.join("..", "autobob", "multilspy")))
-r.add(OtherRepo("serena", "serena", os.path.join("..", "serena", "src", "multilspy")))
+r.add(OtherRepo("serena", "serena", os.path.join("repos", "serena", "src", "multilspy")))
 r.runMain()
